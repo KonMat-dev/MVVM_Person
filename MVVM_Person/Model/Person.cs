57db2e28ea6b364d3b3f075e6674b945a60b6d60
KonMat-dev <konrad.matuszewski.98@gmail.com>
2020-04-28 19:47:56 +0200

    Dodaj pliki projektów.

---
+++ MVVM_Person/Model/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Person.Model
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Weight { get; set; }
+        public int Age { get; set; }
+        public string ALL
+        {
+            get
+            {
+                return ToString();
+            }
+        }
+
+        public Person()
+        {
+            Name = "Test";
+            LastName = "Test32";
+            Weight = 1;
+            Age = 1;
+        }
+
+
+        public Person(string tmp_name, string tmp_lastName, int tmp_weight, int tmp_age)
+        {
+            Name = tmp_name;
+            LastName = tmp_lastName;
+            Weight = tmp_weight;
+            Age = tmp_age;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {LastName}, weight: {Weight}kg, age: {Age}";
+        }
+    }
+}
---
+++ MVVM_Person/Model/Serializacja.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+
+namespace MVVM_Person.Model
+{
+    public static class Serializacja_wczytaj_zapisz
+    {
+        public static void Zapisz(string path, List<Person> person)
+        {
+            XmlSerializer serializacja = new XmlSerializer(typeof(List<Person>));
+
+            using (TextWriter textWriter = new StreamWriter(path))
+            {
+                serializacja.Serialize(textWriter, person);
+            }
+        }
+
+        public static List<Person> Wczytaj(string path)
+        {
+            List<Person> footballers = new List<Person>();
+
+            XmlSerializer serializacja = new XmlSerializer(typeof(List<Person>));
+
+            using (TextReader textWriter = new StreamReader(path))
+            {
+                footballers = serializacja.Deserialize(textWriter) as List<Person>;
+            }
+
+            return footballers;
+        }
+    }
+}
